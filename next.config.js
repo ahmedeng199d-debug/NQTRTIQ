b593c9a5b6fb344ce5b8616a6fb57954f51339a2
GHOSTTRT <ahmedeng199d@gmail.com>
2026-05-10 21:33:58 +0300

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'i.postimg.cc' },
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+    ],
+  },
+  experimental: {
+    optimizePackageImports: ['lucide-react', 'framer-motion'],
+  },
+}
+
+module.exports = nextConfig
